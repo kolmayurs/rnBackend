37a01ad9b3500b1cb3f4daf564ea0db64f86d0ac
Mayur Koli <kolimayurs@gmail.com>
2019-08-12 11:07:11 +0530

    commit

--- userloginui/src/store.js
+++
@@ -1,8 +0,0 @@
-import {createStore, applyMiddleware} from 'redux'; 
-import reducers from './reducers';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-
-const middleware = applyMiddleware(thunk, logger);
-
-export default createStore(reducers, middleware);
