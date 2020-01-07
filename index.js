25efc103f9bcb085247b5e8c6c172692a9bb63f3
Mayur Koli <kolimayurs@gmail.com>
2020-01-07 11:31:29 +0530

    commmit

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const { mongoose } = require('./db/db');
 const userController = require('./controllers/userControllers');
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/user', userController);
@@ -12,3 +26,4 @@ var port=Number(process.env.PORT || 4001);
 app.listen(port, () => {
 	console.log('Product server listing from port ' + port);
 })
+
